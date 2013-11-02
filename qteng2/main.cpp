748fb99b3d57f0d44a72087d9db59217bd6fa0fc
j557 <julia_577@wp.pl>
2013-11-02 16:25:32 +0100

    Added initial main window

--- qteng2/main.cpp
+++ qteng2/main.cpp
@@ -1,14 +1,13 @@
 #include <QtGui/QApplication>
 #include "qmlapplicationviewer.h"
+#include "mainwindow.h"
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
     QScopedPointer<QApplication> app(createApplication(argc, argv));
 
-    QmlApplicationViewer viewer;
-    viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
-    viewer.setMainQmlFile(QLatin1String("qml/qteng2/main.qml"));
-    viewer.showExpanded();
+    MainWindow w;
+    w.show();
 
     return app->exec();
 }
--- qteng2/qteng2.pro
+++ qteng2/qteng2.pro
@@ -30,8 +30,15 @@ symbian:TARGET.CAPABILITY += NetworkServices
 # CONFIG += qt-components
 
 # The .cpp file which was generated for your project. Feel free to hack it.
-SOURCES += main.cpp
+SOURCES += main.cpp \
+    mainwindow.cpp
 
 # Please do not modify the following two lines. Required for deployment.
 include(qmlapplicationviewer/qmlapplicationviewer.pri)
 qtcAddDeployment()
+
+HEADERS += \
+    mainwindow.h
+
+FORMS += \
+    mainwindow.ui
